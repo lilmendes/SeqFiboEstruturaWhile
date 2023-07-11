2fee44bc27cf5de03b4d14d8d8481e5e3a451b2c
Kalil Mendes <102493336+lilmendes@users.noreply.github.com>
2023-07-10 21:48:46 -0300

    Add files via upload

---
+++ SeqMultiplos2UsandoEstruturaWhile/src/pacote/Atvfor.java
@@ -0,0 +1,34 @@
+package pacote;
+
+public class Atvfor {
+
+	public static void main(String[] args) {
+		// elabore uma solução para apresentar uma sequência, construída dinamicamente, com os 12 primeiros  valores múltiplo de 2, 
+	    //com a variável inicializada com valor 1,
+	    //utilizando uma estrutura de repetição com teste no início (while)
+System.out.print("A sequ�ncia vai ser: " );
+//mplemente uma estrutura while para repetir a frase por Ryan 101 vezes.
+int cont = 1, qtd=1;
+while (qtd<=12) {
+	if(cont%2==0) {
+	System.out.println(cont);
+	cont++;
+	qtd++;	
+	}
+	else {
+	cont++;	
+	
+}
+}		
+	
+	
+		
+	}
+
+}	
+		
+		
+		
+	
+		
+	
